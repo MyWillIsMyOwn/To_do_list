17fa312ba22b85cf8ce1b335910f273aa0bd3797
MyWillIsMyOwn <weyerfilip@gmail.com>
2022-05-02 18:51:14 +0200

    Printing items

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import mysql.connector
 
-connect = input("Do you want to connect to database? ")
+try:
+    db = mysql.connector.connect(host='localhost', database='to_do', user='root', password='niema123')
+    cursor = db.cursor()
+    if db.is_connected():
+        print("Connected to {} database".format(db.database))
+except:
+    print("Error while connecting to database")
 
-if connect=="Yes":
-    try:
-        db = mysql.connector.connect(host='localhost',database='to_do',user='root',password='niema123')
-        if db.is_connected():
-            print("Connected to {} database".format(db.database))
-    except:
-        print("Error while connecting to database")
+def print_tasks():
+    cursor.execute("SELECT * FROM tasks")
+    result = cursor.fetchall()
+    for row in result:
+        print("Id: {}, Title: {}, Description: {}".format(row[0],row[1],row[2]))
+    print("")
 
 
-    disconnect = input("To disconnect type 'exit: ")
 
-    if disconnect=="Exit":
-        db.close()
+def add_task():
+    pass
+
+def remove_task():
+    print_tasks()
+
+
+
+
+while True:
+    operation = input("'Add', 'Print', 'Delete', 'Exit' ")
+
+    match operation:
+        case 'Add':
+            pass
+        case 'Print':
+            print_tasks()
+        case 'Delete':
+            remove_task()
+        case 'Exit':
+            db.close()
+            break
